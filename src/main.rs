f6eb4a29ad5aef62e3c8ed464fce35368c7294d7
ambiso <ambisotopy@gmail.com>
2023-07-17 13:29:40 +0200

    Fix stuff

--- src/main.rs
+++ src/main.rs
@@ -27,20 +27,21 @@ struct NonLinearPrng {
 }
 
 impl NonLinearPrng {
-    fn next_8bits(&mut self) -> u32 {
+    fn next_bit(&mut self) -> u32 {
         let iters = self.prng.next();
-        for _ in 0..iters % 2 {
+        for _ in 0..iters & 1 {
             self.prng.next();
         }
         self.prng.next() & 1
     }
 
     fn next(&mut self) -> u32 {
-        let w0 = self.next_8bits();
-        let w1 = self.next_8bits();
-        let w2 = self.next_8bits();
-        let w3 = self.next_8bits();
-        w0 << 24 | w1 << 16 | w2 << 8 | w3
+        let mut w = 0;
+        for _ in 0..32 {
+            w <<= 1;
+            w |= self.next_bit();
+        }
+        w
     }
 }
 
